17ca19ce25400f94c7c2f2a5c8e32267a77ba134
Alex Kearney <hi@kongaloosh.com>
2018-03-13 18:52:02 -0600

    Successfully parses form

---
+++ gst_checker.py
@@ -0,0 +1,20 @@
+import requests
+import re
+import urllib3
+import ssl
+import certifi
+# from mechanize import Browser
+REQUEST_DATE = '2018-01-01'
+# urllib3.contrib.pyopenssl.inject_into_urllib3()
+session = requests.Session()    # we use a session so that the gov site recognizes us as agreeing to terms
+response = session.post('https://www.businessregistration-inscriptionentreprise.gc.ca/ebci/brom/registry/registryservlet', data={'iagree':'yes'})
+#regForm.
+payload = {     # registryPromptSubmit
+        'businessNumber': 863961264,
+        'businessName': 'Kearney Management Consulting Ltd',
+        'requestDate': REQUEST_DATE
+}
+response = session.post(url='https://www.businessregistration-inscriptionentreprise.gc.ca/ebci/brom/registry/registryservlet/ebci/brom/registry/registryPromptSubmit.action',data=payload)
+f = open('test.html', 'w')
+f.write(str(response.content))
+print(response.text)
